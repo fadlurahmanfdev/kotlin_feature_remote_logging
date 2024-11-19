2ab2cf86184964de62ab7f85d649638beb46f2a1
fadlurahmanfdev <taufikfadlurahmanfajari.dev@gmail.com>
2024-11-19 16:35:42 +0700

    add remote logger object

--- remote_logging/src/main/java/com/fadlurahmanfdev/remote_logging/logger/RemoteLogger.kt
+++ remote_logging/src/main/java/com/fadlurahmanfdev/remote_logging/logger/RemoteLogger.kt
@@ -1,5 +1,38 @@
 package com.fadlurahmanfdev.remote_logging.logger
 
-class RemoteLogger {
+import android.util.Log
+import java.util.logging.Level
 
+object RemoteLogger {
+
+    private var listener: Listener? = null
+
+    fun addListener(listener: Listener) {
+        this.listener = listener
+    }
+
+    fun removeListener() {
+        this.listener = null
+    }
+
+    fun log(level: Level, message: String, labels: Map<String, String>?) {
+        when (level) {
+            Level.SEVERE -> {
+                Log.e(this::class.java.simpleName, message)
+            }
+
+            Level.WARNING -> {
+                Log.w(this::class.java.simpleName, message)
+            }
+
+            else -> {
+                Log.i(this::class.java.simpleName, message)
+            }
+        }
+        listener?.log(level, message, labels)
+    }
+
+    interface Listener {
+        fun log(level: Level, message: String, labels: Map<String, String>?)
+    }
 }
